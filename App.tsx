6ec1e535d1fb74d42b19e1c98ac60a43653bc180
Robin van den Berg <robinofski.development@gmail.com>
2024-03-11 23:10:57 +0100

    feat: Refactor App component to use react-native-paper and Home component

--- App.tsx
+++ App.tsx
@@ -1,52 +1,10 @@
-import { StatusBar } from "expo-status-bar";
-import { Button, StyleSheet, Text, View } from "react-native";
-import { useSonglinkApi } from "./src/hooks/";
-import { DataView } from "./src/components/DataView/DataView";
-import { useMemo, useState } from "react";
-import { useShareIntent } from "expo-share-intent";
+import { PaperProvider } from "react-native-paper";
+import { Home } from "./src/components";
 
 export default function App() {
-  const { hasShareIntent, shareIntent, resetShareIntent } = useShareIntent();
-
-  const [songUrl, setSongUrl] = useState<string>("");
-  const { data, loading, error, reset, retry } = useSonglinkApi({
-    songUrl: songUrl,
-  });
-
-  useMemo(() => {
-    if (hasShareIntent && shareIntent.text && shareIntent.text !== songUrl) {
-      setSongUrl(shareIntent.text ?? "");
-      resetShareIntent();
-    }
-  }, [hasShareIntent, shareIntent, resetShareIntent]);
-
   return (
-    <View style={styles.container}>
-      {loading && <Text>Loading...</Text>}
-      {data && <DataView data={data} />}
-      {data || error ? (
-        <Button title="Reset" onPress={reset} />
-      ) : (
-        <Text>Share a song URL to get started</Text>
-      )}
-
-      {loading && <Text>Loading...</Text>}
-      {error && (
-        <>
-          <Text>Error: {error.message}</Text>
-          <Button title="Retry" onPress={() => retry()} />
-        </>
-      )}
-      <StatusBar style="auto" />
-    </View>
+    <PaperProvider>
+      <Home />
+    </PaperProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});

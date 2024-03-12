3ce093066d3683343b6a2bc5f52ee7c34b8b5cbe
Robin van den Berg <robinofski.development@gmail.com>
2024-03-12 22:31:03 +0100

    chore: Refactor DataView component to use Card component

--- src/components/DataView/DataView.tsx
+++ src/components/DataView/DataView.tsx
@@ -1,7 +1,7 @@
 import { View, Linking, StyleSheet, Image } from "react-native";
 import { SonglinkApiResponse } from "../../types";
 import { useShare } from "../../hooks";
-import { Button, Surface, Text } from "react-native-paper";
+import { Button, Card, IconButton, Surface, Text } from "react-native-paper";
 
 interface Props {
   data: SonglinkApiResponse | null;
@@ -15,56 +15,79 @@ export const DataView = ({ data }: Props) => {
   }
 
   return (
-    <Surface elevation={1} style={styles.container}>
-      <Image
+    <Card style={styles.card}>
+      <Card.Cover
+        style={styles.coverImage}
         source={{
           uri: data.entitiesByUniqueId[data.entityUniqueId].thumbnailUrl,
         }}
-        style={styles.coverImage}
       />
-      <Text variant="bodyLarge">
-        {data.entitiesByUniqueId[data.entityUniqueId].artistName}
-      </Text>
-      <Text variant="titleMedium">
-        {data.entitiesByUniqueId[data.entityUniqueId].title}
-      </Text>
-      <View style={styles.buttonRow}>
-        <Button
-          mode="contained"
-          icon={"share"}
-          onPress={() => {
-            share(data.pageUrl);
-          }}
-        >
-          Share
-        </Button>
-        <Button onPress={() => Linking.openURL(data.pageUrl)}>
-          Go to Songlink website
-        </Button>
-      </View>
-    </Surface>
+      <Card.Content style={styles.cardContent}>
+        <Text variant="titleLarge">
+          {data.entitiesByUniqueId[data.entityUniqueId].title}
+        </Text>
+        <Text variant="bodyMedium">
+          {data.entitiesByUniqueId[data.entityUniqueId].artistName}
+        </Text>
+        <Card.Actions>
+          <Button mode="text" onPress={() => Linking.openURL(data.pageUrl)}>
+            Songlink website
+          </Button>
+          <Button
+            mode="contained"
+            icon={"share"}
+            onPress={() => {
+              share(data.pageUrl);
+            }}
+          >
+            Share
+          </Button>
+        </Card.Actions>
+      </Card.Content>
+    </Card>
   );
+
+  // return (
+  //   <Surface elevation={1} style={styles.container}>
+  //     <Image
+  //       source={{
+  //         uri: data.entitiesByUniqueId[data.entityUniqueId].thumbnailUrl,
+  //       }}
+  //       style={styles.coverImage}
+  //     />
+  //     <Text variant="bodyLarge">
+  //       {data.entitiesByUniqueId[data.entityUniqueId].artistName}
+  //     </Text>
+  //     <Text variant="titleMedium">
+  //       {data.entitiesByUniqueId[data.entityUniqueId].title}
+  //     </Text>
+  //     <View style={styles.buttonRow}>
+  //       <Button
+  //         mode="contained"
+  //         icon={"share"}
+  //         onPress={() => {
+  //           share(data.pageUrl);
+  //         }}
+  //       >
+  //         Share
+  //       </Button>
+  //       <Button onPress={() => Linking.openURL(data.pageUrl)}>
+  //         Go to Songlink website
+  //       </Button>
+  //     </View>
+  //   </Surface>
+  // );
 };
 
 const styles = StyleSheet.create({
-  container: {
-    display: "flex",
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-    padding: 10,
-    borderRadius: 8,
+  card: {
+    width: 300,
   },
   coverImage: {
-    marginBottom: 10,
-    width: 100,
-    height: 100,
-    borderRadius: 8,
+    width: 300,
+    height: 300,
   },
-  buttonRow: {
-    marginTop: 10,
-    display: "flex",
-    flexDirection: "row",
-    gap: 10,
+  cardContent: {
+    padding: 16,
   },
 });

7a4727c3d78d80ae576dd1add7975c6dc3c03d99
Robin van den Berg <robinofski.development@gmail.com>
2024-03-12 21:44:47 +0100

    feat: Add Material3 theme support

--- App.tsx
+++ App.tsx
@@ -1,11 +1,25 @@
-import { PaperProvider } from "react-native-paper";
+import { MD3DarkTheme, MD3LightTheme, PaperProvider } from "react-native-paper";
 import { Home } from "./src/components";
 import { SafeAreaProvider } from "react-native-safe-area-context";
+import { useColorScheme } from "react-native";
+import { useMaterial3Theme } from "@pchmn/expo-material3-theme";
+import { useMemo } from "react";
 
 export default function App() {
+  const colorScheme = useColorScheme();
+  const { theme } = useMaterial3Theme();
+
+  const paperTheme = useMemo(
+    () =>
+      colorScheme === "dark"
+        ? { ...MD3DarkTheme, colors: theme.dark }
+        : { ...MD3LightTheme, colors: theme.light },
+    [colorScheme, theme]
+  );
+
   return (
     <SafeAreaProvider>
-      <PaperProvider>
+      <PaperProvider theme={paperTheme}>
         <Home />
       </PaperProvider>
     </SafeAreaProvider>
